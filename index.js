e2166d47a463113f01eaf66edfbb95e2a60596f8
Emilio Orduña <emilio.ordunap@hotmail.com>
2024-06-04 00:48:13 +0200

    basic events defined

--- index.js
+++ index.js
@@ -7,17 +7,34 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
+const connectedUsers = new Map();
 
 io.on('connection', (socket) => {
-    socket.on('joinRoom', (roomId) => {
-      socket.join(roomId);
+    socket.on('joinRoom', () => {
+      const userId = socket.handshake.query.userId;
+      const roomId = socket.handshake.query.roomId;
+
+      
+
       // Send welcome message to the room
-      socket.emit('welcome', `Bienvenido a la sala ${roomId}`);
+      socket.emit('welcome', `Bienvenido a la sala ${roomId} usuario con id:${userId}`);
+      socket.join(roomId);
+
+      connectedUsers.set(socket.id, {idUserConectado: userId, roomIdConectado: roomId });
     });
   
-    socket.on('message', (message) => {
-        
-    })
+    socket.on('sendMessage', (message) => {
+      const socketConectado = connectedUsers.get(socket.id);
+      const usuarioID = socketConectado.userId;
+      socket.to(socketConectado.roomIdConectado).emit('messageReceived', {message, usuarioID});
+    });
+
+      // Handle user disconnection when the socket disconnects
+    socket.on('disconnect', () => {
+      connectedUsers.delete(socket.id);
+    });
+
+
   });
   
   // Escuchar en el puerto 3000 (o cualquier puerto de tu elección)
